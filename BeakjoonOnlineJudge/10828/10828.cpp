e3b59d2214f1bdd8f3695f4252d34ad44c508562
Tak <staktree000@gmail.com>
2021-03-08 22:44:23 +0900

    [#03]BOJ 10828번 스택

--- BeakjoonOnlineJudge/10828/10828.cpp
+++ BeakjoonOnlineJudge/10828/10828.cpp
@@ -79,6 +79,10 @@ void excuteCommand(string comm)
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+
+    cin.tie(NULL);
+
     settingCommand();
     cin >> N;
     for(int i = 0; i < N; i++)
